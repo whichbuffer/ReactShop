6801f68fdba53086d54fd90f38333d0b1ec16bff
Arda Büyükkaya <42712921+whichbuffer@users.noreply.github.com>
2020-10-23 17:41:02 +0300

    route

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home(){
         <div className="hello">
             <h1 class="animate__animated animate__bounce">Hello,welcome to React Shop</h1>
             <img class=" hello App-logo react-logo" src="logo192.png"></img>
-            <p className="hello font-p">this is a simple ecommerce website for understanding concepts of ReactJS and CSS </p>
+            <p className="hello font-p">this is a simple e-commerce website for understanding concepts of ReactJS and CSS </p>
         </div>
         
 
